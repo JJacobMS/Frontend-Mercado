d21e7f75c77bc0c7a52a5c550b98310f7d7f0bf5
JJacobMS <montielsalasjesus@gmail.com>
2024-12-12 13:14:45 -0600

    Usuarios Tamaño

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -9,10 +9,12 @@ public class Usuario
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     [EmailAddress(ErrorMessage = "El campo {0} no es correo valido.")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
     public required string Email { get; set; }
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "El campo {0} no puede contener solo espacios.")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
     public required string Nombre { get; set; }
 
     public string? Rol { get; set; }
--- Models/UsuarioPwd.cs
+++ Models/UsuarioPwd.cs
@@ -7,6 +7,7 @@ public class UsuarioPwd
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
     [EmailAddress(ErrorMessage = "El campo {0} no es correo valido.")]
     [Display(Name = "Correo electronico")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
     public required string Email { get; set; }
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
@@ -14,9 +15,11 @@ public class UsuarioPwd
     [Display(Name = "Contraseña")]
     [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?""{}|<>[\]\\/_=;:.-]).{8,}$",
     ErrorMessage = "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, un número y un carácter especial.")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
     public required string Password { get; set; }
     [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "El campo {0} no puede contener solo espacios.")]
 
     [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "El campo {0} debe tener entre {2} y {1} caracteres.")]
     public required string Nombre { get; set; }
 }
